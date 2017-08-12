f50b4d5609030ff53d2a9a1990ae318e93094635
nnoldman <1527053436@qq.com>
2017-08-12 20:07:09 +0800

    登陆游戏服

--- CommandLine.cpp
+++ CommandLine.cpp
@@ -5,13 +5,15 @@ namespace Basic
 
 CommandLine::CommandLine(int narg, const char** args)
 {
-    for (int i = 1; i < narg; ++i) {
-        uString element = args[i];
+    for (int i = 1; i < narg; ++i)
+    {
+        std::string element = args[i];
         size_t pos = element.find("=");
-        if (pos != uString::npos) {
-            uString key = element.substr(0, pos);
-            uString value = element.substr(pos + 1);
-            mArgs.Insert(key, value);
+        if (pos != uString::npos)
+        {
+            std::string key = element.substr(0, pos);
+            std::string value = element.substr(pos + 1);
+            args_.Insert(key, value);
         }
     }
 }
@@ -22,6 +24,6 @@ CommandLine::~CommandLine()
 
 size_t CommandLine::arg_count()
 {
-    return mArgs.size();
+    return args_.size();
 }
 }
